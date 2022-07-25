7d7e61c73ec310e518f123de65ab08163444e86a
Jellef Abbenseth <jellef@gmx.de>
2022-07-25 19:48:04 +0200

    Remastering Portfolio Website
    - adding name and links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  alttext: string = "under-construction";
-  path: string = "./assets/under-construction.png";
+  linkedIn = "https://www.linkedin.com/in/jellef-abbenseth"
+  linkedInImage = "https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"
+  xing = "https://www.xing.com/profile/Jellef_Abbenseth"
+  xingImage = "https://zangano.de/wp-content/uploads/2019/05/xing-icon-logo-png-transparent-450x529.png"
+  gitHub = "https://github.com/JellefAbbenseth"
+  gitHubImage = "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/92-icloud-512.png"
+  //alttext: string = "under-construction";
+  //path: string = "./assets/under-construction.png";
 }
