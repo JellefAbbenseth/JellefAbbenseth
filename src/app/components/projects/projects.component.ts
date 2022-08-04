63f2c75a5c6875f9fd950c6a52178326d06ce02a
Jellef Abbenseth <jellef@gmx.de>
2022-08-04 15:58:57 +0200

    Remastering Portfolio Website
    - added projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, DoCheck {
+  schoolProjectImage = "/assets/project-photos/Home_User.jpg"
+  growthV2Image = "/assets/project-photos/growthV2.jpg"
+  growthImage = "/assets/project-photos/growth.jpg"
+  portfolioImage = "/assets/project-photos/portfolio_mainpage.jpg"
 
-  constructor() { }
+  title = "Projects"
+  language = "eng"
+  language$: Observable<string>;
 
-  ngOnInit(): void {
+  constructor(private store: Store<{ information: string }>) {
+    this.language$ = store.select('information')
   }
 
+  ngOnInit(): void {
+    this.language$.subscribe(language => this.language = language)
+    this.setTexts()
+  }
+
+  ngDoCheck(): void {
+    this.setTexts()
+  }
+
+  setTexts() {
+    if(this.language == "eng") this.title = "Projects"
+    else this.title = "Projekte"
+  }
 }
